926bca294871dd4de828261c595dc79e8df1b0ae
ussumane <ussumane.momade@uem.ac.mz>
2024-04-11 18:25:15 +0200

    fix eslint errors

--- 0x02-ES6_classes/5-main.js
+++ 0x02-ES6_classes/5-main.js
@@ -4,18 +4,17 @@ const b = new Building(100);
 console.log(b);
 
 class TestBuilding extends Building {
-    constructor(sqft) {
-        super(sqft)
-    }
+  constructor(sqft) {
+    super(sqft);
+  }
 
-    evacuationWarningMessage() {
-        console.log("OKAY")
-    }
+  evacuationWarningMessage() {
+    console.log('OKAY');
+  }
 }
 
 try {
-    new TestBuilding(200)
-}
-catch(err) {
-    console.log(err);
+  new TestBuilding(200);
+} catch (err) {
+  console.log(err);
 }
